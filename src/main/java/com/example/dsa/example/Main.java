ace238e644c9ce8c2fdcad32f621e0889bcfff56
swasteee <swasteekhadka@gmail.com>
2024-08-19 01:40:22 +0545

    inital

---
+++ src/main/java/com/example/dsa/example/Main.java
@@ -0,0 +1,7 @@
+package com.example.dsa.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/example/dsa/example/test.java
@@ -0,0 +1,9 @@
+package com.example.dsa.example;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println(
+
+        );
+    }
+}
